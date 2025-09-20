1539cdbf9d23f0abd1674f212da7e14cb7f6683b
Dmitrii Kovanikov <kovanikov@gmail.com>
2025-09-20 19:40:54 +0100

    Finish Homework2 solutions

--- Homework2/main.cpp
+++ Homework2/main.cpp
@@ -1,7 +1,10 @@
+#include <algorithm>
 #include <cassert>
 #include <charconv>
+#include <fstream>
 #include <iostream>
 #include <optional>
+#include <memory>
 #include <print>
 #include <ranges>
 #include <string>
@@ -21,6 +24,18 @@ struct Error {
 };
 
 using MessageType = std::variant<Info, Warning, Error>;
+
+struct MessageTypeToStringVisitor {
+    std::string operator()(Info) const { return "I"; }
+    std::string operator()(Warning) const { return "W"; }
+    std::string operator()(Error error) const {
+        return std::format("E {}", error.code);
+    }
+};
+
+std::string toString(const MessageType& msgType) {
+    return std::visit(MessageTypeToStringVisitor{}, msgType);
+}
 
 using Timestamp = int;
 
@@ -44,6 +59,20 @@ LogMessage make_message(MessageType type, Timestamp timestamp, std::string msg) 
     return Message{ type, timestamp, std::move(msg) };
 }
 
+struct LogMessageToStringVisitor {
+    std::string operator()(const Message& msg) const {
+        return std::format("{} {} {}", toString(msg.type), msg.timestamp, msg.msg);
+    }
+
+    std::string operator()(const Unknown& unknown) const {
+        return std::format("Unknown: {}", unknown.msg);
+    }
+};
+
+std::string toString(const LogMessage& logMessage) {
+    return std::visit(LogMessageToStringVisitor{}, logMessage);
+}
+
 std::optional<int> to_int(std::string_view sv)
 {
     int r;
@@ -55,26 +84,20 @@ std::optional<int> to_int(std::string_view sv)
 }
 
 std::string unwords(const std::vector<std::string>& words, int to_drop) {
-    auto words_pipe = words
+    return words
         | std::views::drop(to_drop)
-        | std::views::join_with(' ');
-
-    return std::string(words_pipe.begin(), words_pipe.end());
+        | std::views::join_with(' ')
+        | std::ranges::to<std::string>();
 }
 
 LogMessage parseMessage(std::string str) {
     // Step 1: Split into words
-    auto words_pipe = str
+    std::vector<std::string> words = str
         | std::views::split(' ')
         | std::views::transform([](const auto& range) {
             return std::string{range.begin(), range.end()};
-        });
-
-    // Step 2: Combine words
-    std::vector<std::string> words;
-    for (auto&& word : words_pipe) {
-        words.push_back(std::move(word));
-    };
+        })
+        | std::ranges::to<std::vector>();
 
     auto get = [&words](size_t i) -> std::optional<std::string> {
         if (i < words.size()) {
@@ -124,6 +147,118 @@ LogMessage parseMessage(std::string str) {
         .value_or(Unknown{str});
 }
 
+std::string readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary); // open in binary to avoid newline translation
+    return std::string((std::istreambuf_iterator<char>(file)),
+                         std::istreambuf_iterator<char>());
+}
+
+std::vector<LogMessage> parseFile(std::string filename) {
+    std::string file_contents = readFile(filename);
+    return file_contents
+        | std::views::split('\n')
+        | std::views::transform([](const auto& line) -> LogMessage {
+          return parseMessage(std::string{line.begin(), line.end()});
+        })
+        | std::ranges::to<std::vector>();
+}
+
+/*
+data MessageTree = Leaf | Node MessageTree LogMessage MessageTree
+*/
+
+struct Leaf {};
+struct Node;
+
+using MessageTree = std::variant<
+    Leaf,
+    std::unique_ptr<Node>
+>;
+
+struct Node {
+    Message msg;
+    MessageTree left;
+    MessageTree right;
+};
+
+struct InsertVisit {
+    Message msg;
+
+    MessageTree operator()(Leaf) const {
+        return std::make_unique<Node>(msg, Leaf{}, Leaf{});
+    }
+
+    MessageTree operator()(std::unique_ptr<Node>& node) const {
+        Timestamp cur_value = node->msg.timestamp;
+        if (msg.timestamp < cur_value) {
+            node->left = std::visit(*this, node->left);
+        } else if (msg.timestamp > cur_value) {
+            node->right = std::visit(*this, node->right);
+        }
+        return std::move(node);
+    }
+};
+
+MessageTree insert(const Message& message, MessageTree& tree) {
+    return std::visit(InsertVisit{message}, tree);
+}
+
+MessageTree insert(MessageTree& tree, const LogMessage& logMessage) {
+    if (const auto* _ = std::get_if<Unknown>(&logMessage)) {
+        return std::move(tree);
+    }
+
+    const auto* message = std::get_if<Message>(&logMessage);
+    return insert(*message, tree);
+}
+
+MessageTree build(const std::vector<LogMessage>& messages) {
+    return std::ranges::fold_left(messages, Leaf{},
+        [](MessageTree tree, const LogMessage& logMessage) {
+            return insert(tree, logMessage);
+        }
+    );
+}
+
+struct InOrderVisit {
+    std::vector<Message> messages;
+
+    void operator()(Leaf) {}
+
+    void operator()(const std::unique_ptr<Node>& node) {
+        std::visit(*this, node->left);
+        messages.push_back(node->msg);
+        std::visit(*this, node->right);
+    }
+};
+
+std::vector<Message> in_order(const MessageTree& tree) {
+    auto visitor = InOrderVisit{std::vector<Message>()};
+    std::visit(visitor, tree);
+    return visitor.messages;
+}
+
+void what_went_wrong(std::string file_name) {
+    auto logMessages = parseFile(file_name);
+    auto messageTree = build(logMessages);
+    auto relevantMessages = in_order(messageTree);
+
+    auto importantMessages = relevantMessages
+        | std::views::filter([](const Message& msg) {
+            if (const auto* error = std::get_if<Error>(&msg.type)) {
+                return error->code >= 50;
+            }
+            return false;
+        })
+        | std::views::transform([](const Message& msg) {
+            return msg.msg;
+        });
+
+    for (auto&& msg : importantMessages) {
+        std::println("{}", msg);
+    }
+}
+
 int main() {
     std::println("========= Start Here =========");
 
@@ -135,4 +270,5 @@ int main() {
       LogMessage{ Unknown{ "This is not in the right format" } }));
     std::println("========= PASS: 'parseMessage' tests =========");
 
+    what_went_wrong("Homework2/sample.log");
 }
