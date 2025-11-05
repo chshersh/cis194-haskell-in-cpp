e9481bb4f7f4bfbdd2dba62ac244fe73af83a35b
Dmitrii Kovanikov <kovanikov@gmail.com>
2025-11-05 22:52:59 +0000

    Finish Homework4

--- Homework4/main.cpp
+++ Homework4/main.cpp
@@ -27,8 +27,37 @@ struct Tree {
 };
 
 template <typename T>
-std::unique_ptr<Tree<T>> fold_tree(const std::vector<T>& vec) {
-    return nullptr;
+int height(const std::unique_ptr<Tree<T>>& tree) {
+    return tree ? tree->height : 0;
+}
+
+template <typename T>
+void print_tree(const std::unique_ptr<Tree<T>>& tree, std::string padding) {
+    if (!tree) return;
+    std::println("{}{}", padding, tree->val);
+    print_tree(tree->left, padding + "  ");
+    print_tree(tree->right, padding + "  ");
+}
+
+template <typename T>
+std::unique_ptr<Tree<T>> build_tree(std::span<T> items) {
+    auto build = [items](auto self, size_t l, size_t r) -> std::unique_ptr<Tree<T>> {
+        if (l >= r) return nullptr;
+
+        size_t mid = l + (r - l) / 2;
+        auto left  = self(self, l, mid);
+        auto right = self(self, mid + 1, r);
+        int h = 1 + std::max(height(left), height(right));
+
+        return std::make_unique<Tree<T>>(
+            h,
+            items[mid],
+            std::move(left),
+            std::move(right)
+        );
+    };
+
+    return build(build, 0, items.size());
 }
 
 int func1(std::span<int> xs) {
@@ -78,4 +107,9 @@ int main() {
     std::array arr = {1, 8, 3, 4, 5};
     std::println("Func1: {}", func1(arr));
     std::println("Primes: {}", sieve_sundaram(16));
+
+    std::array items = {1, 3, 5, 7, 8, 10};
+    auto tree = build_tree(std::span{items.begin(), items.end()});
+    std::println("Here!");
+    print_tree(tree, "");
 }
